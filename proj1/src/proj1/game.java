c0593f76e12ef48551c98a573c3605aa4f4b6fd3
michaelbrady6 <michael.brady108@gmail.com>
2016-09-19 09:24:14 -0600

    I got a completely random grid

--- proj1/src/proj1/game.java
+++ proj1/src/proj1/game.java
@@ -1,39 +1,64 @@
 package proj1;
 import java.util.Scanner;
-
+import java.util.ArrayList;
 public class game
 	{
+		static String coordinates;
+		static int row;
+		static int col;
+		static String[][] grid=new String[4][4];
+		static String[][] blankGrid={{" ", " ", " ", " "}, {" ", " ", " ", " "}, {" ", " ", " ", " "}, {" ", " ", " ", " "}};
 		public static void main(String[] args)
 			{
-				Scanner input=new Scanner(System.in);
 				
 				//sets answer positions
 				int answers=(int)(Math.random()*99)+1;
-				char[][] grid=new char[2][2];
+				
 				boolean[][] shown={{false, false}, {false, false}};
-				if(answers<50)
-				{
-					grid[0][0]='y';
-					grid[0][1]='x';
-					grid[1][0]='x';
-					grid[1][1]='y';
-					
-				}
-				else
-				{
-					grid[0][0]='x';
-					grid[0][1]='y';
-					grid[1][0]='y';
-					grid[1][1]='x';
-				}
-				printGrid(grid, shown);
+//				if(answers<25)
+//				{
+//					grid[0][0]="y";
+//					grid[0][1]="x";
+//					grid[1][0]="x";
+//					grid[1][1]="y";
+//				}
+//				else if(answers<50)
+//					{
+//						grid[0][0]="y";
+//						grid[0][1]="x";
+//						grid[1][0]="y";
+//						grid[1][1]="x";
+//					}
+//				else if(answers<75)
+//				{
+//					grid[0][0]="x";
+//					grid[0][1]="y";
+//					grid[1][0]="y";
+//					grid[1][1]="x";
+//				}
+//				else if(answers<100)
+//					{
+//						grid[0][0]="x";
+//						grid[0][1]="y";
+//						grid[1][0]="x";
+//						grid[1][1]="y";
+//					}
+//				
+//				printGrid(grid, shown);
+//				coordinatesToNumbers();
+//				printGrid(grid, shown);
+				gridRandomizing();
 			}
-		public static void printGrid(char[][] l, boolean[][] f)
+		public static void printGrid(String[][] l, boolean[][] f)
 			{
-				System.out.println("  1 2");
-//				System.out.println("A        |       ");
-//				System.out.println("  ---------------");
-//				System.out.println("B        |       ");
+				System.out.println("  1 2 3 4");
+				System.out.println("A  | | | |");
+				System.out.println("  --------");
+				System.out.println("B  | | | |");
+				System.out.println("  --------");
+				System.out.println("C  | | | |");
+				System.out.println("  --------");
+				System.out.println("C  | | | |");
 				for(int i=0; i<2; i++)
 				{
 					if(i==0){
@@ -50,11 +75,74 @@ public class game
 							System.out.print(l[i][j]+" ");
 							
 						}
-					}
-					
+					}					
 					System.out.println();
 				}
 			}
+		public static void coordinatesToNumbers()
+			{
+				Scanner input=new Scanner(System.in);
+				coordinates = input.nextLine().toUpperCase();
+				switch(coordinates.substring(0,1))
+				{
+					case "A":
+					{
+						row = 0;
+						break;
+					}
+					case "B":
+					{
+						row = 1;
+						break;
+					}
+					case "C":
+					{
+						row = 2;
+						break;
+					}
+					case "D":
+					{
+						row = 3;
+						break;
+					}
+				}
+				col = Integer.parseInt(coordinates.substring(1))-1;
+				blankGrid[row][col] = grid[row][col]; 
+			}
+		public static void gridRandomizing()
+		{
+			ArrayList<String> cards = new ArrayList<String>();
+			cards.add("y");
+			cards.add("y");
+			cards.add("z");
+			cards.add("z");
+			cards.add("x");
+			cards.add("x");
+			cards.add("p");
+			cards.add("p");
+			cards.add("a");
+			cards.add("a");
+			cards.add("f");
+			cards.add("f");
+			cards.add("k");
+			cards.add("k");
+			cards.add("q");
+			cards.add("q");
+			int k = 0;
+			for (int i = 0; i<4; i++)
+				{
+					for (int j = 0; j<4; j++)
+						{
+							
+							int random = (int) (Math.random()*(16 - k));
+							grid[i][j] = cards.get(random);
+							cards.remove(random);
+							System.out.print(grid[i][j] + " ");
+							k++;
+						}
+					System.out.println();
+				}
+		}
 		private static void allTrue(boolean[][] f){
 			
 		}
